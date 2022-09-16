5d78971794772840a9edf54c4db8f3771c81465e
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-09-16 16:20:38 -0400

    Review JavaScript: let & const

---
+++ LetAndConst/index.js
@@ -0,0 +1,27 @@
+// function iterateVar() {
+//    for(var i = 0; i < 10; i++) {
+//       console.log(i);
+//    }
+//
+//    console.log(i);
+// }
+
+// function iterateLet() {
+//    for(let i = 0; i < 10; i++) {
+//       console.log(i);
+//    }
+//
+//    console.log(i);
+// }
+
+// iterateVar();
+// iterateLet();
+
+function reinitConst() {
+  const me = 1;
+  console.log(me);
+  me = 2;
+  console.log(me);
+}
+
+reinitConst();
