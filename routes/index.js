8800a88d6a59f3362fb28f888bd9988818a87694
Lanh Hoang <lanhhoang@users.noreply.github.com>
2022-09-28 20:10:45 -0400

    Week-03: Explain EJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Home" });
+});
+
+router.get("/service", function (req, res, next) {
+  res.render("index", { title: "Service" });
+});
+
+router.get("/julio", function (req, res, next) {
+  res.render("main", { title: "Julio" });
 });
 
 module.exports = router;
